8c175a6c20f2c1cb4c3133a3abd1640a0f820aab
José Pinto <zepinto@gmail.com>
2022-05-31 14:45:05 +0100

    ComapIntegrationPanel: Now also adding move tasks.

--- plugins-dev/comap/src/java/pt/omst/comap/ComapIntegrationPanel.java
+++ plugins-dev/comap/src/java/pt/omst/comap/ComapIntegrationPanel.java
@@ -1,21 +1,25 @@
 package pt.omst.comap;
 
+import java.util.Optional;
 import java.util.Vector;
 
 import pt.lsts.imc.GeoFeature;
 import pt.lsts.imc.MapPoint;
+import pt.lsts.imc.MoveTask;
 import pt.lsts.imc.SurveyTask;
 import pt.lsts.imc.SynchAdmin;
-import pt.lsts.imc.TaskAdim;
-import pt.lsts.imc.WorldModel;
 import pt.lsts.imc.SynchAdmin.OP;
+import pt.lsts.imc.TaskAdim;
+import pt.lsts.imc.TaskAdminArgs;
+import pt.lsts.imc.WorldModel;
 import pt.lsts.imc.def.SensorType;
+import pt.lsts.neptus.NeptusLog;
 import pt.lsts.neptus.comm.manager.imc.ImcMsgManager;
 import pt.lsts.neptus.console.ConsoleLayout;
 import pt.lsts.neptus.console.ConsolePanel;
 import pt.lsts.neptus.plugins.PluginDescription;
 import pt.lsts.neptus.types.coord.LocationType;
-import pt.lsts.neptus.types.map.AbstractElement;
+import pt.lsts.neptus.types.map.MarkElement;
 import pt.lsts.neptus.types.map.PathElement;
 
 /**
@@ -26,39 +30,66 @@ import pt.lsts.neptus.types.map.PathElement;
 public class ComapIntegrationPanel extends ConsolePanel {
 
     private WorldModel model = null;
+    int feature_id = 1;
 
     public ComapIntegrationPanel(ConsoleLayout console) {
-        super(console);        
+        super(console);
+    }
+
+    private Optional<TaskAdminArgs> getTask(int id) {
+        if (model == null)
+            return Optional.empty();
+        for (GeoFeature f : model.getGeoFeatures()) {
+            if (f.getFeatureId() == id) {
+                if (f.getPoints().size() > 1) {
+                    return Optional.of(new SurveyTask(id, id, SensorType.SIDESCAN, 750,
+                            System.currentTimeMillis() / 1000.0 + 1800));
+                } else if (f.getPoints().size() == 1) {
+                    return Optional
+                            .of(new MoveTask(id, f.getPoints().get(0), System.currentTimeMillis() / 1000.0 + 1800));
+                }
+            }
+        }
+        return Optional.empty();
     }
 
     synchronized WorldModel getModel() {
-        if (model == null) {
-            int feature_id = 1;
-            model = new WorldModel();
-            Vector<GeoFeature> features = new Vector<>();
+        feature_id = 1;
+        model = new WorldModel();
+        Vector<GeoFeature> features = new Vector<>();
 
-            for (PathElement el : getConsole().getMission().generateMapGroup().getAllObjectsOfType(PathElement.class)) {
-                System.out.println("processing "+el.getId());
-                Vector<LocationType> points = el.getShapePoints();
-                System.out.println(points);
-                if (points == null || points.size() <3)
-                    continue;
-                Vector<MapPoint> featurePoints = new Vector<>();
-                GeoFeature feature = new GeoFeature();
-                feature.setFeatureId(feature_id++);
-                for (LocationType loc : points)
-                    featurePoints.add(new MapPoint(loc.getLatitudeRads(), loc.getLongitudeRads(), 0));
-                feature.setPoints(featurePoints);
-                features.add(feature);
-            }
-            model.setGeoFeatures(features);
+        for (PathElement el : getConsole().getMission().generateMapGroup().getAllObjectsOfType(PathElement.class)) {
+            Vector<LocationType> points = el.getShapePoints();
+            if (points == null || points.size() < 3)
+                continue;
+            Vector<MapPoint> featurePoints = new Vector<>();
+            GeoFeature feature = new GeoFeature();
+            feature.setFeatureId(feature_id++);
+            for (LocationType loc : points)
+                featurePoints.add(new MapPoint(loc.getLatitudeRads(), loc.getLongitudeRads(), 0));
+            feature.setPoints(featurePoints);
+            features.add(feature);
         }
+
+        for (MarkElement el : getConsole().getMission().generateMapGroup().getAllObjectsOfType(MarkElement.class)) {
+            GeoFeature feature = new GeoFeature();
+            Vector<MapPoint> featurePoints = new Vector<>();
+            featurePoints.add(new MapPoint(el.getCenterLocation().getLatitudeRads(),
+                    el.getCenterLocation().getLongitudeRads(), 0));
+            feature.setPoints(featurePoints);
+            feature.setFeatureId(feature_id++);
+            features.add(feature);
+        }
+
+        model.setGeoFeatures(features);
+
+        System.out.println(model.asJSON());
+        feature_id = 1;
         return model;
     }
 
-    static int feature_id = 1;
     @Override
-    public void initSubPanel() {        
+    public void initSubPanel() {
         addMenuItem("Tools>CoMap>Send SynchAdmin/HOLD", null, a -> {
             SynchAdmin msg = new SynchAdmin();
             msg.setOp(OP.HOLD);
@@ -70,33 +101,33 @@ public class ComapIntegrationPanel extends ConsolePanel {
             msg.setOp(OP.RESUME);
             ImcMsgManager.getManager().sendMessageToSystem(msg, getConsole().getMainSystem());
         });
-    
+
         addMenuItem("Tools>CoMap>Send World Model", null, a -> {
             ImcMsgManager.getManager().sendMessageToSystem(getModel(), getConsole().getMainSystem());
         });
 
-        addMenuItem("Tools>CoMap>Send TaskAdmin Assign", null, a -> {
+        addMenuItem("Tools>CoMap>Assign Task", null, a -> {
             TaskAdim msg = new TaskAdim();
             msg.setTid(feature_id);
             msg.setOp(TaskAdim.OP.ASSIGN);
-            SurveyTask st = new SurveyTask(feature_id, feature_id, SensorType.SIDESCAN, 750, System.currentTimeMillis() / 1000.0 + 3600.0);
-            msg.setArg(st);
+            Optional<TaskAdminArgs> arg = getTask(feature_id);
+            if (arg.isPresent())
+                msg.setArg(getTask(feature_id).get());
             feature_id++;
             ImcMsgManager.getManager().sendMessageToSystem(msg, getConsole().getMainSystem());
         });
 
-        addMenuItem("Tools>CoMap>Send TaskAdmin Unassign", null, a -> {
+        addMenuItem("Tools>CoMap>Unassign Task", null, a -> {
             feature_id--;
             TaskAdim msg = new TaskAdim();
             msg.setTid(feature_id);
             msg.setOp(TaskAdim.OP.UNASSIGN);
             ImcMsgManager.getManager().sendMessageToSystem(msg, getConsole().getMainSystem());
         });
-        
     }
 
     @Override
-    public void cleanSubPanel() {        
+    public void cleanSubPanel() {
         
     }
 
